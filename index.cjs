61d56b7e674908b9e105dfb0ca15b5c8e8e513bd
js8126302 <122713123+js8126302@users.noreply.github.com>
2025-03-03 16:57:57 -0800

    Attaching backend to databse

--- index.cjs
+++ index.cjs
@@ -18,6 +18,29 @@ app.get('/api/notes', (req, res) => {
 
 })
 
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = new Note ({
+    content: body.content,
+    important: body.important || false, 
+  })
+  note.save().then(savedNote => response.json(savedNote))
+})
+
+//find note by id
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  Note.findById(id).then(note => {
+    note ? response.json(note) : response.json({message: `Note with id: ${id} is not found `})
+  }).catch (error => response.status(500).json({message: 'Error finding note', error}))
+})
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
@@ -29,26 +52,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-})
 
 
  
@@ -61,16 +65,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
